47983d6a8593b469b9b239638766b332af904d84
ry_Tom <31386783+02ryo15@users.noreply.github.com>
2020-03-02 04:29:21 +0900

    update files

--- src/main/java/ryoryo/polishedlib/block/BlockBaseWall.java
+++ src/main/java/ryoryo/polishedlib/block/BlockBaseWall.java
@@ -13,8 +13,9 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
+import ryoryo.polishedlib.util.interfaces.IModId;
 
-public class BlockBaseWall extends BlockWall
+public abstract class BlockBaseWall extends BlockWall implements IModId
 {
 	public BlockBaseWall(Block baseBlock)
 	{
@@ -28,21 +29,21 @@ public class BlockBaseWall extends BlockWall
 		this.setUnlocalizedName(name + "_wall");
 	}
 
-	private boolean canConnectTo(IBlockAccess worldIn, BlockPos pos)
+	private boolean canConnectTo(IBlockAccess world, BlockPos pos)
 	{
-		IBlockState iblockstate = worldIn.getBlockState(pos);
+		IBlockState iblockstate = world.getBlockState(pos);
 		Block block = iblockstate.getBlock();
 		return block == Blocks.BARRIER ? false : (block != this && !(block instanceof BlockFenceGate) && !(block instanceof BlockWall) ? (block.getMaterial(iblockstate).isOpaque() && iblockstate.isFullCube() ? block.getMaterial(iblockstate) != Material.GOURD : false) : true);
 	}
 
-	public IBlockState getActualState(IBlockState state, IBlockAccess worldIn, BlockPos pos)
+	public IBlockState getActualState(IBlockState state, IBlockAccess world, BlockPos pos)
 	{
-		boolean flag = this.canConnectTo(worldIn, pos.north());
-		boolean flag1 = this.canConnectTo(worldIn, pos.east());
-		boolean flag2 = this.canConnectTo(worldIn, pos.south());
-		boolean flag3 = this.canConnectTo(worldIn, pos.west());
+		boolean flag = this.canConnectTo(world, pos.north());
+		boolean flag1 = this.canConnectTo(world, pos.east());
+		boolean flag2 = this.canConnectTo(world, pos.south());
+		boolean flag3 = this.canConnectTo(world, pos.west());
 		boolean flag4 = flag && !flag1 && flag2 && !flag3 || !flag && flag1 && !flag2 && flag3;
-		return state.withProperty(UP, Boolean.valueOf(!flag4 || !worldIn.isAirBlock(pos.up()))).withProperty(NORTH, Boolean.valueOf(flag)).withProperty(EAST, Boolean.valueOf(flag1)).withProperty(SOUTH, Boolean.valueOf(flag2)).withProperty(WEST, Boolean.valueOf(flag3));
+		return state.withProperty(UP, Boolean.valueOf(!flag4 || !world.isAirBlock(pos.up()))).withProperty(NORTH, Boolean.valueOf(flag)).withProperty(EAST, Boolean.valueOf(flag1)).withProperty(SOUTH, Boolean.valueOf(flag2)).withProperty(WEST, Boolean.valueOf(flag3));
 	}
 
 	@Override
