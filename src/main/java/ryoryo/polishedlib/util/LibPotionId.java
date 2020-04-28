08be29b8c02a738b232cbbc34af9e7a2f3efb734
ry_Tom <31386783+02ryo15@users.noreply.github.com>
2020-04-28 23:29:02 +0900

    move from polished stone

---
+++ src/main/java/ryoryo/polishedlib/util/LibPotionId.java
@@ -0,0 +1,61 @@
+package ryoryo.polishedlib.util;
+
+import net.minecraft.potion.Potion;
+
+public class LibPotionId
+{
+	/** 移動速度上昇 */
+	public static final int SPEED = Potion.getIdFromPotion(Potions.SPEED);
+	/** 移動速度低下 */
+	public static final int SLOWNESS = Potion.getIdFromPotion(Potions.SLOWNESS);
+	/** 採掘速度上昇*/
+	public static final int HASTE = Potion.getIdFromPotion(Potions.HASTE);
+	/** 採掘速度低下 */
+	public static final int MINING_FATIGUE = Potion.getIdFromPotion(Potions.MINING_FATIGUE);
+	/** 攻撃力上昇*/
+	public static final int STRENGTH = Potion.getIdFromPotion(Potions.STRENGTH);
+	/** 即時回復 */
+	public static final int INSTANT_HEALTH = Potion.getIdFromPotion(Potions.INSTANT_HEALTH);
+	/** 負傷 */
+	public static final int INSTANT_DAMAGE = Potion.getIdFromPotion(Potions.INSTANT_DAMAGE);
+	/** ジャンプ力上昇 */
+	public static final int JUMP_BOOST = Potion.getIdFromPotion(Potions.JUMP_BOOST);
+	/** 吐き気 */
+	public static final int NAUSEA = Potion.getIdFromPotion(Potions.NAUSEA);
+	/** 再生 */
+	public static final int REGENERATION = Potion.getIdFromPotion(Potions.REGENERATION);
+	/** 耐性 */
+	public static final int RESISTANCE = Potion.getIdFromPotion(Potions.RESISTANCE);
+	/** 火炎耐性 */
+	public static final int FIRE_RESISTANCE = Potion.getIdFromPotion(Potions.FIRE_RESISTANCE);
+	/** 水中呼吸 */
+	public static final int WATER_BREATHING = Potion.getIdFromPotion(Potions.WATER_BREATHING);
+	/** 透明化 */
+	public static final int INVISIBILITY = Potion.getIdFromPotion(Potions.INVISIBILITY);
+	/** 盲目 */
+	public static final int BLINDNESS = Potion.getIdFromPotion(Potions.BLINDNESS);
+	/** 暗視 */
+	public static final int NIGHT_VISION = Potion.getIdFromPotion(Potions.NIGHT_VISION);
+	/** 空腹 */
+	public static final int HUNGER = Potion.getIdFromPotion(Potions.HUNGER);
+	/** 弱体化 */
+	public static final int WEAKNESS = Potion.getIdFromPotion(Potions.WEAKNESS);
+	/** 毒 */
+	public static final int POISON = Potion.getIdFromPotion(Potions.POISON);
+	/** ウィザー */
+	public static final int WITHER = Potion.getIdFromPotion(Potions.WITHER);
+	/** 体力増強 */
+	public static final int HEALTH_BOOST = Potion.getIdFromPotion(Potions.HEALTH_BOOST);
+	/** 衝撃吸収 */
+	public static final int ABSORPTION = Potion.getIdFromPotion(Potions.ABSORPTION);
+	/** 満腹度回復 */
+	public static final int SATURATION = Potion.getIdFromPotion(Potions.SATURATION);
+	/** 発光 */
+	public static final int GLOWING = Potion.getIdFromPotion(Potions.GLOWING);
+	/** 浮遊 */
+	public static final int LEVITATION = Potion.getIdFromPotion(Potions.LEVITATION);
+	/** 幸運 */
+	public static final int LUCK = Potion.getIdFromPotion(Potions.LUCK);
+	/** 不運 */
+	public static final int UNLUCK = Potion.getIdFromPotion(Potions.UNLUCK);
+}
